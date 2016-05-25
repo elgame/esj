9911ef7cef2e02d571f83c4fa9da600779c033f2
Elgame <gamalielmendozasolis@gmail.com>
2016-05-25 15:12:42 -0500

    respaldo

--- PostgresqlBack/crea_respaldos.bat
+++ PostgresqlBack/crea_respaldos.bat
@@ -2,6 +2,6 @@
 :: cuenta mandrill empaquesanjorgemx@gmail.com S4nj0rg3V14n3y
 :: cuenta Dropbox empaquesanjorgemx@gmail.com S4nj0rg3V14n3y
 
-ROBOCOPY C:\xampp\htdocs\sanjorge C:\Dropbox\sanjorge /E /V /XO /LOG:C:\Users\Administrator\Documents\log_respaldo_app.txt
+ROBOCOPY C:\xampp\htdocs\sanjorge C:\Dropbox\sanjorge /E /V /XO /XD C:\xampp\htdocs\sanjorge\application\logs C:\xampp\htdocs\sanjorge\application\media\bascula_snap C:\xampp\htdocs\sanjorge\application\media\polizas /LOG:C:\Users\Administrator\Documents\log_respaldo_app.txt
 
 ROBOCOPY C:\xampp\htdocs\Respaldos C:\Dropbox\Respaldos /E /V /XO /LOG:C:\Users\Administrator\Documents\log_respaldo_resp.txt
