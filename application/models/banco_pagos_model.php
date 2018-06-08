d3a49d07f7cb27b762d0dc50a4342e7e68a61b0a
Gama <gamalielmendozasolis@gmail.com>
2018-06-08 14:20:18 -0500

    fix pago masivo

--- application/models/banco_pagos_model.php
+++ application/models/banco_pagos_model.php
@@ -357,6 +357,10 @@ class banco_pagos_model extends CI_Model {
       }
     }
 
+    $cuentaa = explode('-', $cuenta_retiro->cuenta);
+    if (count($cuentaa) == 3)
+      $cuenta_retiro->cuenta = $cuentaa[1];
+
     $data = array(
       //Reg de Control
       'id_cuenta' => $cuenta_retiro->id_cuenta,
