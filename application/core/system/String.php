bd1768d23fdbb03c70e4050c578f256898bbe261
Elgame <gamalielmendozasolis@gmail.com>
2018-06-09 14:48:44 -0500

    fix string

--- application/core/system/String.php
+++ application/core/system/String.php
@@ -606,27 +606,24 @@ class String{
   }
 
   public static function ultimoDia($anho,$mes){
-    echo "<pre>";
-       var_dump($mes);
-     echo "</pre>";exit;
      if (((fmod($anho,4)==0) and (fmod($anho,100)!=0)) or (fmod($anho,400)==0)) {
          $dias_febrero = 29;
      } else {
          $dias_febrero = 28;
      }
      switch($mes) {
-         case 01: return 31; break;
-         case 02: return $dias_febrero; break;
-         case 03: return 31; break;
-         case 04: return 30; break;
-         case 05: return 31; break;
-         case 06: return 30; break;
-         case 07: return 31; break;
-         case 08: return 31; break;
-         case 09: return 30; break;
-         case 10: return 31; break;
-         case 11: return 30; break;
-         case 12: return 31; break;
+         case '01': return 31; break;
+         case '02': return $dias_febrero; break;
+         case '03': return 31; break;
+         case '04': return 30; break;
+         case '05': return 31; break;
+         case '06': return 30; break;
+         case '07': return 31; break;
+         case '08': return 31; break;
+         case '09': return 30; break;
+         case '10': return 31; break;
+         case '11': return 30; break;
+         case '12': return 31; break;
      }
   }
 
