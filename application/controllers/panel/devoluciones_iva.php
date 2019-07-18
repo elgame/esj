ac73e2e2c00824b960dba0021feaeb1210a0df7c
elgame <gamalielmendozasolis@gmail.com>
2019-07-17 23:25:39 -0500

    Inicio reportes de dev de iva

---
+++ application/controllers/panel/devoluciones_iva.php
@@ -0,0 +1,108 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class devoluciones_iva extends MY_Controller {
+  /**
+   * Evita la validacion (enfocado cuando se usa ajax). Ver mas en privilegios_model
+   * @var unknown_type
+   */
+  private $excepcion_privilegio = array(
+    'devoluciones_iva/cedula_proveedores_pdf/',
+    'devoluciones_iva/cedula_proveedores_xls/',
+
+    'devoluciones_iva/cproductos_pdf/',
+    'devoluciones_iva/cproductos_xls/',
+    'devoluciones_iva/cproducto_pdf/',
+  );
+
+
+  public function _remap($method)
+  {
+    $this->load->model("usuarios_model");
+    if($this->usuarios_model->checkSession()){
+      $this->usuarios_model->excepcion_privilegio = $this->excepcion_privilegio;
+      $this->info_empleado                         = $this->usuarios_model->get_usuario_info($this->session->userdata('id_usuario'), true);
+
+      if($this->usuarios_model->tienePrivilegioDe('', get_class($this).'/'.$method.'/')){
+        $this->{$method}();
+      }else
+        redirect(base_url('panel/home?msg=1'));
+    }else
+      redirect(base_url('panel/home'));
+  }
+
+  public function index()
+  {
+    # code...
+  }
+
+  public function cedula_proveedores()
+  {
+    $this->carabiner->js(array(
+      array('general/msgbox.js'),
+      array('panel/almacen/rpt_compras.js'),
+    ));
+
+    $this->load->library('pagination');
+    $this->load->model('empresas_model');
+
+    $params['info_empleado']  = $this->info_empleado['info'];
+    $params['seo']        = array('titulo' => 'CÉDULA A DETALLE DE PROVEEDORES SOLICITADOS');
+
+    $params['empresa'] = $this->empresas_model->getDefaultEmpresa();
+
+    if(isset($_GET['msg']{0}))
+      $params['frm_errors'] = $this->showMsgs($_GET['msg']);
+
+    $this->load->view('panel/header',$params);
+    $this->load->view('panel/devoluciones_iva/cedula_proveedores',$params);
+    $this->load->view('panel/footer',$params);
+  }
+  public function cedula_proveedores_pdf(){
+    $this->load->model('devoluciones_iva_model');
+    $this->devoluciones_iva_model->getCedulaProveedoresPdf();
+  }
+  public function cedula_proveedores_xls(){
+    $this->load->model('devoluciones_iva_model');
+    $this->devoluciones_iva_model->getCedulaProveedoresXls();
+  }
+
+  public function cproductos()
+  {
+    $this->carabiner->js(array(
+      array('general/msgbox.js'),
+      array('panel/almacen/rpt_compras.js'),
+    ));
+
+    $this->load->library('pagination');
+    $this->load->model('empresas_model');
+
+    $params['info_empleado']  = $this->info_empleado['info'];
+    $params['seo']        = array('titulo' => 'Compras por Producto');
+
+    $params['empresa'] = $this->empresas_model->getDefaultEmpresa();
+
+    if(isset($_GET['msg']{0}))
+      $params['frm_errors'] = $this->showMsgs($_GET['msg']);
+
+    $this->load->view('panel/header',$params);
+    $this->load->view('panel/almacen/compras/cproducto',$params);
+    $this->load->view('panel/footer',$params);
+  }
+  public function cproductos_pdf(){
+    $this->load->model('devoluciones_iva_model');
+    $this->devoluciones_iva_model->getCProductosPdf();
+  }
+  public function cproductos_xls(){
+    $this->load->model('devoluciones_iva_model');
+    $this->devoluciones_iva_model->getCProductosXls();
+  }
+  public function cproducto_pdf()
+  {
+    $this->load->model('devoluciones_iva_model');
+    $this->devoluciones_iva_model->getCProductoPdf();
+  }
+
+
+}
+
+?>
---
+++ application/views/panel/devoluciones_iva/cedula_proveedores.php
@@ -0,0 +1,98 @@
+    <div class="span3">
+      <!-- content starts -->
+      <div class="row-fluid">
+        <div class="box span12">
+          <div class="box-content">
+            <form action="<?php echo base_url('panel/devoluciones_iva/cedula_proveedores_pdf/'); ?>" method="GET" class="form-search" id="frmrptcproform" target="frame_reporte">
+              <div class="form-actions form-filters">
+
+                <div class="control-group span6">
+                  <label class="control-label" for="ffecha1">Del</label>
+                  <div class="controls">
+                    <input type="date" name="ffecha1" class="span11" id="ffecha1" value="<?php echo isset($_GET['ffecha1']) ? $_GET['ffecha1'] : date('Y-m-01'); ?>">
+                  </div>
+                </div>
+
+                <div class="control-group span6">
+                  <label class="control-label" for="ffecha2">Al</label>
+                  <div class="controls">
+                    <input type="date" name="ffecha2" class="span11" id="ffecha2" value="<?php echo isset($_GET['ffecha2']) ? $_GET['ffecha2'] : date('Y-m-d'); ?>">
+                  </div>
+                </div>
+
+                <div class="control-group">
+                  <label class="control-label" for="dempresa">Empresa</label>
+                  <div class="controls">
+                    <input type="text" name="dempresa"
+                      value="<?php echo (isset($empresa->nombre_fiscal)? $empresa->nombre_fiscal: ''); ?>" id="dempresa" class="span12" placeholder="Nombre">
+                    <input type="hidden" name="did_empresa" value="<?php echo (isset($empresa->id_empresa)? $empresa->id_empresa: ''); ?>" id="did_empresa">
+                  </div>
+                </div>
+
+                <div class="control-group">
+                  <label class="control-label" for="fproducto">Proveedores</label>
+                  <div class="controls">
+                    <div class="input-append span12">
+                      <input type="text" name="dproveedor" value="" id="dproveedor" class="span9" placeholder="Buscar">
+                      <button class="btn" type="button" id="btnAddProveedor" style="margin-left:-3px;"><i class="icon-plus-sign"></i></button>
+                      <input type="hidden" name="did_proveedor" value="" id="did_proveedor">
+                    </div>
+                    <div class="clearfix"></div>
+                    <div style="height:130px;overflow-y: scroll;background-color:#eee;">
+                      <ul id="lista_proveedores" style="list-style: none;margin-left: 4px;">
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="form-actions">
+                  <button type="submit" class="btn btn-primary btn-large span12">Enviar</button>
+                </div>
+
+              </div>
+            </form> <!-- /form -->
+
+          </div>
+        </div><!--/span12 -->
+      </div><!--/row-fluid -->
+    </div><!-- /span3 -->
+
+    <div id="content" class="span9">
+      <!-- content starts -->
+
+      <div class="row-fluid">
+        <div class="box span12">
+          <a href="" id="linkDownXls" data-url="<?php echo base_url('panel/devoluciones_iva/cedula_proveedores_xls'); ?>" class="linksm" target="_blank">
+            <i class="icon-table"></i> Excel</a>
+
+          <div class="box-content">
+            <div class="row-fluid">
+              <iframe id="frame_reporte" name="frame_reporte" src="<?php echo base_url('panel/devoluciones_iva/cedula_proveedores_pdf/'); ?>" style="width: 100%;height: 475px;"></iframe>
+            </div>
+          </div>
+        </div><!--/span-->
+      </div><!--/row-->
+
+    </div><!--/#content.span9-->
+
+
+
+<?php if (isset($p) && isset($pe)) { ?>
+  <script>
+    var win=window.open(<?php echo "'".base_url('panel/bascula/imprimir_pagadas/?'.MyString::getVarsLink(array('msg', 'p', 'pe')).'&pe='.$pe)."'" ?>, '_blank');
+    win.focus();
+  </script>
+<?php } ?>
+
+<!-- Bloque de alertas -->
+<?php if(isset($frm_errors)){
+  if($frm_errors['msg'] != ''){
+?>
+<script type="text/javascript" charset="UTF-8">
+  $(document).ready(function(){
+    noty({"text":"<?php echo $frm_errors['msg']; ?>", "layout":"topRight", "type":"<?php echo $frm_errors['ico']; ?>"});
+  });
+</script>
+<?php }
+}?>
+<!-- Bloque de alertas -->
