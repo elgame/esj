7a2df1989cc808a0729129168c0480aa8a9dd983
elgame <gamalielmendozasolis@gmail.com>
2019-07-20 13:09:57 -0500

    Terminado reporte de Totalidad de iva y Integracion iva 16%

--- application/controllers/panel/devoluciones_iva.php
+++ application/controllers/panel/devoluciones_iva.php
@@ -8,10 +8,10 @@ class devoluciones_iva extends MY_Controller {
   private $excepcion_privilegio = array(
     'devoluciones_iva/cedula_proveedores_pdf/',
     'devoluciones_iva/cedula_proveedores_xls/',
-
-    'devoluciones_iva/cproductos_pdf/',
-    'devoluciones_iva/cproductos_xls/',
-    'devoluciones_iva/cproducto_pdf/',
+    'devoluciones_iva/cedula_totalidad_iva_pdf/',
+    'devoluciones_iva/cedula_totalidad_iva_xls/',
+    'devoluciones_iva/cedula_iva16_pdf/',
+    'devoluciones_iva/cedula_iva16_xls/',
   );
 
 
@@ -65,7 +65,7 @@ class devoluciones_iva extends MY_Controller {
     $this->devoluciones_iva_model->getCedulaProveedoresXls();
   }
 
-  public function cedula_proveedores()
+  public function cedula_totalidad_iva()
   {
     $this->carabiner->js(array(
       array('general/msgbox.js'),
@@ -75,8 +75,8 @@ class devoluciones_iva extends MY_Controller {
     $this->load->library('pagination');
     $this->load->model('empresas_model');
 
-    $params['info_empleado']  = $this->info_empleado['info'];
-    $params['seo']        = array('titulo' => 'CÉDULA A DETALLE DE PROVEEDORES SOLICITADOS');
+    $params['info_empleado'] = $this->info_empleado['info'];
+    $params['seo']           = array('titulo' => 'CEDULA INTEGRACION TOTALIDAD DE IVA');
 
     $params['empresa'] = $this->empresas_model->getDefaultEmpresa();
 
@@ -84,36 +84,30 @@ class devoluciones_iva extends MY_Controller {
       $params['frm_errors'] = $this->showMsgs($_GET['msg']);
 
     $this->load->view('panel/header',$params);
-    $this->load->view('panel/devoluciones_iva/cedula_proveedores',$params);
+    $this->load->view('panel/devoluciones_iva/cedula_totalidad_iva',$params);
     $this->load->view('panel/footer',$params);
   }
-  public function cedula_proveedores_pdf(){
+  public function cedula_totalidad_iva_pdf(){
     $this->load->model('devoluciones_iva_model');
-    $this->devoluciones_iva_model->getCedulaProveedoresXls(true);
+    $this->devoluciones_iva_model->getCedulaTotalidadIvaXls(true);
   }
-  public function cedula_proveedores_xls(){
+  public function cedula_totalidad_iva_xls(){
     $this->load->model('devoluciones_iva_model');
-    $this->devoluciones_iva_model->getCedulaProveedoresXls();
+    $this->devoluciones_iva_model->getCedulaTotalidadIvaXls();
   }
 
-
-
-
-
-
-
-  public function cproductos()
+  public function cedula_iva16()
   {
     $this->carabiner->js(array(
       array('general/msgbox.js'),
-      array('panel/almacen/rpt_compras.js'),
+      array('panel/rpt_dev_iva/rpt_dev_iva.js'),
     ));
 
     $this->load->library('pagination');
     $this->load->model('empresas_model');
 
-    $params['info_empleado']  = $this->info_empleado['info'];
-    $params['seo']        = array('titulo' => 'Compras por Producto');
+    $params['info_empleado'] = $this->info_empleado['info'];
+    $params['seo']           = array('titulo' => 'CEDULA INTEGRACION IVA 16%');
 
     $params['empresa'] = $this->empresas_model->getDefaultEmpresa();
 
@@ -121,21 +115,16 @@ class devoluciones_iva extends MY_Controller {
       $params['frm_errors'] = $this->showMsgs($_GET['msg']);
 
     $this->load->view('panel/header',$params);
-    $this->load->view('panel/almacen/compras/cproducto',$params);
+    $this->load->view('panel/devoluciones_iva/cedula_iva16',$params);
     $this->load->view('panel/footer',$params);
   }
-  public function cproductos_pdf(){
+  public function cedula_iva16_pdf(){
     $this->load->model('devoluciones_iva_model');
-    $this->devoluciones_iva_model->getCProductosPdf();
+    $this->devoluciones_iva_model->getCedulaIva16Xls(true);
   }
-  public function cproductos_xls(){
+  public function cedula_iva16_xls(){
     $this->load->model('devoluciones_iva_model');
-    $this->devoluciones_iva_model->getCProductosXls();
-  }
-  public function cproducto_pdf()
-  {
-    $this->load->model('devoluciones_iva_model');
-    $this->devoluciones_iva_model->getCProductoPdf();
+    $this->devoluciones_iva_model->getCedulaIva16Xls();
   }
 
 
