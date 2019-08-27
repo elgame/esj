886c147cd8ef0b0aac7f782149c5f821a1b44bc4
elgame <gamalielmendozasolis@gmail.com>
2019-08-27 15:25:25 -0500

    notas

--- application/scripts/panel/rastreabilidad/admin.js
+++ application/scripts/panel/rastreabilidad/admin.js
@@ -19,4 +19,20 @@ $(function(){
     var $this = $(this);
     $this.attr('href', $this.data('href')+$this.data('id')+'&serie='+$('#serieRemisionar').val());
   });
+
+  // Autocomplete Empresas
+  $("#dempresa").autocomplete({
+    source: base_url + 'panel/bascula/ajax_get_empresas/',
+    minLength: 1,
+    selectFirst: true,
+    select: function( event, ui ) {
+      $("#did_empresa").val(ui.item.id);
+      $("#dempresa").val(ui.item.label).css({'background-color': '#99FF99'});
+    }
+  }).keydown(function(e){
+    if (e.which === 8) {
+      $(this).css({'background-color': '#FFD9B3'});
+      $('#did_empresa').val('');
+    }
+  });
 });
