7f7deffc42b571ce2a3dcd187b2f48eddb831a65
elgame <gamalielmendozasolis@gmail.com>
2020-09-23 16:07:55 -0500

    bancomer, empleado

--- application/views/panel/empleados/agregar.php
+++ application/views/panel/empleados/agregar.php
@@ -311,7 +311,8 @@
                     <select name="fbanco" id="fbanco">
                       <option value="bancr" <?php echo set_select('fbanco', 'bancr', false, $this->input->post('fbanco')); ?>>BBVA Bancomer</option>
                       <option value="santr" <?php echo set_select('fbanco', 'santr', false, $this->input->post('fbanco')); ?>>Santander</option>
-                      <option value="banor" <?php echo set_select('fbanco', 'banor', false, $this->input->post('fbanco')); ?>>Santander</option>
+                      <option value="banor" <?php echo set_select('fbanco', 'banor', false, $this->input->post('fbanco')); ?>>Banorte</option>
+                      <option value="efectivo" <?php echo set_select('fbanco', 'efectivo', false, $this->input->post('fbanco')); ?>>Efectivo</option>
                     </select>
                     </div>
                   </div>
