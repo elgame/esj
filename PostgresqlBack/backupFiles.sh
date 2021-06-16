6d823551fcd0408838323d86f11032f20965f32e
elgame <gamalielmendozasolis@gmail.com>
2021-06-16 18:20:05 -0500

    backup

---
+++ PostgresqlBack/backupFiles.sh
@@ -0,0 +1,5 @@
+#rsync -a --delete /var/www/html/test /mnt/sda3/Backups/
+rsync -a --delete /var/www/html/sanjorge /mnt/sda3/Backups/
+rsync -a --delete /var/www/html/sanjorge_bascula /mnt/sda3/Backups/
+rsync -a --delete /var/www/html/sanjorge /media/backupserver/
+
