8d4c59af3a57739bbc6e839f02f3f115746cb4a7
elgame <gamalielmendozasolis@gmail.com>
2021-09-28 11:03:54 -0500

    mostrar productos certificados

--- application/models/bodega_guadalajara_model.php
+++ application/models/bodega_guadalajara_model.php
@@ -67,10 +67,10 @@ class bodega_guadalajara_model extends CI_Model {
       {
         $info['existencia_dia'][$value->id_factura.'-'.$value->id_clasificacion.'-'.$value->id_unidad.'-'.$key] = clone $value;
       }
-      if ( GastosProductos::searchGastosProductos($value->id_clasificacion) )
-      {
-        unset($info['remisiones'][$key]);
-      }
+      // if ( GastosProductos::searchGastosProductos($value->id_clasificacion) )
+      // {
+      //   unset($info['remisiones'][$key]);
+      // }
     }
 
     // sumamos o restamos los prestamos de ese dia
