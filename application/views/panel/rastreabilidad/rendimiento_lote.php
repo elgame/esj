28647782c6e664461997c1fd4e22999fbe131e74
elgame <gamalielmendozasolis@gmail.com>
2021-09-29 13:49:56 -0500

    imprimir rendimientos de limon

--- application/views/panel/rastreabilidad/rendimiento_lote.php
+++ application/views/panel/rastreabilidad/rendimiento_lote.php
@@ -18,6 +18,7 @@
           <div class="box-header well" data-original-title>
             <h2><i class="icon-bar-chart"></i> Rendimiento por Lote</h2>
             <div class="box-icon">
+              <a href="<?php echo base_url('panel/rastreabilidad/rpt_lotes_pdf/?fecha='.$fecha); ?>" class="btn btn-round btn-danger" title="Imprimir reporte de lotes" target="_BLANK"><i class="icon-print"></i></a>
               <a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
             </div>
           </div>
