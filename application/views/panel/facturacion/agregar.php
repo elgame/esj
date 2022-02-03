788367d718997413ca7042e8d6144d337b67f9a0
elgame <gamalielmendozasolis@gmail.com>
2022-02-02 18:15:21 -0600

    Fix cp carta porte 5

--- application/views/panel/facturacion/agregar.php
+++ application/views/panel/facturacion/agregar.php
@@ -3020,7 +3020,7 @@
 ?>
 <script type="text/javascript" charset="UTF-8">
   $(document).ready(function(){
-    noty({"text":"<?php echo addslashes($frm_errors['msg']); ?>", "layout":"topRight", "type":"<?php echo $frm_errors['ico']; ?>"});
+    noty({"text":"<?php echo str_replace("\\n", " ", addslashes($frm_errors['msg'])); ?>", "layout":"topRight", "type":"<?php echo $frm_errors['ico']; ?>"});
   });
 </script>
 <?php }
