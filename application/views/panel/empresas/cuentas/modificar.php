bd36e0e9026b5b4d178ac4932222adc4ea41831f
elgame <gamalielmendozasolis@gmail.com>
2022-03-02 19:26:27 -0600

    cuenta cpi

--- application/views/panel/empresas/cuentas/modificar.php
+++ application/views/panel/empresas/cuentas/modificar.php
@@ -89,8 +89,8 @@
                         <option value="IsrRetPagadoHono" <?php echo set_select('dtipo_cuenta', 'IsrRetPagadoHono', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Isr Ret Pagado Honorarios</option>
                         <option value="IsrRetXPagar" <?php echo set_select('dtipo_cuenta', 'IsrRetXPagar', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Isr Ret Por Pagar</option>
                         <option value="IsrRetPagado" <?php echo set_select('dtipo_cuenta', 'IsrRetPagado', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Isr Ret Pagado</option>
-                        <option value="IsrRetXPagar125" <?php echo set_select('dtipo_cuenta', 'IsrRetPagado', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Isr Ret Por Pagar 1.25%</option>
-                        <option value="IsrRetPagado125" <?php echo set_select('dtipo_cuenta', 'IsrRetPagado', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Isr Ret Pagado 1.25%</option>
+                        <option value="IsrRetXPagar125" <?php echo set_select('dtipo_cuenta', 'IsrRetXPagar125', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Isr Ret Por Pagar 1.25%</option>
+                        <option value="IsrRetPagado125" <?php echo set_select('dtipo_cuenta', 'IsrRetPagado125', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Isr Ret Pagado 1.25%</option>
 
                         <option value="NCGasto" <?php echo set_select('dtipo_cuenta', 'NCGasto', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Rebajas Y Bonificaciones Gastos</option>
                         <option value="CuadreGasto" <?php echo set_select('dtipo_cuenta', 'CuadreGasto', false, (isset($cuenta['info']->tipo_cuenta)?$cuenta['info']->tipo_cuenta:'')); ?>>Cuenta Cuadre Gasto</option>
