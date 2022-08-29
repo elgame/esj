29f256b516d1e7a34351b8a35297bf1019999751
elgame <gamalielmendozasolis@gmail.com>
2022-08-29 15:44:08 -0500

    gastos caja

--- application/config/config_user.php
+++ application/config/config_user.php
@@ -15,13 +15,13 @@
 |http://localhost/sanjorge/   http://sanjorge.dev/
 */
 
-$config['jsv']  = '1.215';
+$config['jsv']  = '1.220';
 $config['snapshot_cam1']  = 'http://Bascula:Basc.2021$.@192.168.1.243/Streaming/channels/1/picture';
 $config['snapshot_cam2']  = 'http://Bascula:Basc.2021$.@192.168.1.244/Streaming/channels/1/picture';
 $config['snapshot_cam3']  = 'http://admin:12345@192.168.1.241/Streaming/channels/1/picture';
 $config['snapshot_cam4']  = 'http://admin:12345@192.168.1.242/Streaming/channels/1/picture';
 
-$config['is_bodega'] = 1;
+$config['is_bodega'] = 0;
 
 // configuracion de clasificaciones unidas
 $config['clasif_joins']  = ['2' => '29', '29' => '2'];
