bedbdeaeaf503a36daf5c741c53e12ed6a967c11
elgame <gamalielmendozasolis@gmail.com>
2023-05-22 11:37:00 -0600

    conf camara

--- application/config/config_user.php
+++ application/config/config_user.php
@@ -17,7 +17,8 @@
 
 $config['jsv']  = '1.710';
 $config['snapshot_cam1']  = 'http://Bascula:Basc.2021$.@192.168.1.243/Streaming/channels/1/picture';
-$config['snapshot_cam2']  = 'http://Bascula:Basc.2021$.@192.168.1.244/Streaming/channels/1/picture';
+$config['snapshot_cam2']  = 'http://Bascula:Basc.2021$.@192.168.1.244/ISAPI/Streaming/channels/101/picture';
+// $config['snapshot_cam2']  = 'http://Bascula:Basc.2021$.@192.168.1.244/Streaming/channels/1/picture';
 $config['snapshot_cam3']  = 'http://admin:12345@192.168.1.241/Streaming/channels/1/picture';
 $config['snapshot_cam4']  = 'http://admin:12345@192.168.1.242/Streaming/channels/1/picture';
 
