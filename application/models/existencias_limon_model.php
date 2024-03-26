453234fbea9dbbb4c313865147f84df761d53fc0
elgame <gamalielmendozasolis@gmail.com>
2024-03-26 12:05:16 -0600

    Existencia quite piso 2

--- application/models/existencias_limon_model.php
+++ application/models/existencias_limon_model.php
@@ -495,17 +495,14 @@ class existencias_limon_model extends CI_Model {
     {
       $info['existencia_anterior'] = $existencia_anterior->result();
     }
-    // // Quitamos la existencia anterior de piso
-    // foreach ($info['existencia_anterior'] as $key => $item) {
-    //   foreach ($info['existencia_piso_anterior'] as $key1 => $value) {
-    //     if (($item->id_calibre.$item->id_unidad) == ($value->id_calibre.$value->id_unidad)) {
-    //       $info['existencia_anterior'][$key]->cantidad -= round($value->cantidad, 2);
-    //       $info['existencia_anterior'][$key]->kilos    -= round($value->kilos, 2);
-    //       $info['existencia_anterior'][$key]->importe  -= round($value->importe, 2);
-    //     }
-    //   }
-    // }
 
+    // Quitamos la existencia anterior de piso agregándola a reproceso
+    foreach ($info['existencia_piso_anterior'] as $key1 => $value) {
+      $value->cantidad *= -1;
+      $value->kilos *= -1;
+      $value->importe *= -1;
+      $info['existencia_reproceso'][] = $value;
+    }
 
     $existencia = [];
     foreach ($info['existencia_anterior'] as $key => $item) {
